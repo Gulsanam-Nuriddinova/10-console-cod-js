562557d6008f4df5025be11201c823a0739d607a
Gulsanam-Nuriddinova <gulsanamnuriddinova56@gmail.com>
2024-12-16 05:11:12 -0800

    init

---
+++ js/app.js
@@ -0,0 +1,68 @@
+
+//1
+//let name = prompt("Ismingizni kiriting")
+
+//console.log("Salom" +" " + name);
+
+
+
+//2
+// let son1  = prompt("1-sonni kiriting")
+// let son2  = prompt("2-sonni kiriting")
+
+// console.log(Number(son1) + Number(son2));
+// console.log(Number(son1) - Number(son2));
+// console.log(Number(son1) * Number(son2));
+// console.log(Number(son1) / Number(son2));
+
+//3
+// let name = prompt("Ismingizni kirting")
+// let age = prompt("Yoshingizni kirting")
+
+// console.log( "Salom"+" "+"mening"+" "+"ismim"+" "+name+","+" "+"yoshim"+" "+Number(age)+"da")
+
+//4
+// let son = "42"
+// console.log(Number(son))
+
+//5
+// let son1 = 15
+// console.log(String(son1));
+
+//6
+// let a = "true"
+
+// console.log(Boolean(a));
+
+
+//7
+// let son1  = prompt("1-sonni kiriting")
+//  let son2  = prompt("2-sonni kiriting")
+
+// console.log(Number(son1) > Number(son2));
+// console.log(Number(son1) < Number(son2));
+// console.log(Number(son1) == Number(son2));
+
+
+//8
+// let a = prompt("Tug'ilgan yilingizni kirting")
+// let b = "2024"
+
+// console.log(Number(b)-Number(a));
+
+
+//9
+// let a = prompt("To'liq ismingizni kiriting")
+
+
+// console.log( a.toUpperCase());
+// console.log( a.toLowerCase());
+// console.log(a.length);
+// console.log(a.charAt(0));
+
+
+// 10
+// let a = prompt("Matn kiriting")
+
+// console.log(a.trim());
+
