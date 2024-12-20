87114b07f27d17e8bef5820015b28c46335d0f6e
Gulsanam-Nuriddinova <gulsanamnuriddinova56@gmail.com>
2024-12-20 02:11:08 -0800

    init

--- js/app.js
+++ js/app.js
@@ -15,24 +15,33 @@
 // console.log(Number(son1) * Number(son2));
 // console.log(Number(son1) / Number(son2));
 
+
+
 //3
 // let name = prompt("Ismingizni kirting")
 // let age = prompt("Yoshingizni kirting")
 
 // console.log( "Salom"+" "+"mening"+" "+"ismim"+" "+name+","+" "+"yoshim"+" "+Number(age)+"da")
 
+
+
 //4
 // let son = "42"
 // console.log(Number(son))
+
+
 
 //5
 // let son1 = 15
 // console.log(String(son1));
 
+
+
 //6
 // let a = "true"
 
 // console.log(Boolean(a));
+
 
 
 //7
@@ -44,6 +53,7 @@
 // console.log(Number(son1) == Number(son2));
 
 
+
 //8
 // let a = prompt("Tug'ilgan yilingizni kirting")
 // let b = "2024"
@@ -51,9 +61,10 @@
 // console.log(Number(b)-Number(a));
 
 
+
+
 //9
 // let a = prompt("To'liq ismingizni kiriting")
-
 
 // console.log( a.toUpperCase());
 // console.log( a.toLowerCase());
@@ -61,8 +72,13 @@
 // console.log(a.charAt(0));
 
 
+
+
 // 10
 // let a = prompt("Matn kiriting")
 
 // console.log(a.trim());
+
+
+
 
